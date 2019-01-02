2651f5576a5de74f665d5f8102250a7a9fbdfa5c
f-s-plus-plus <fsaulean@uvm.edu>
2019-01-01 22:53:24 -0500

    add comments

--- index.php
+++ index.php
@@ -26,22 +26,38 @@
       </nav>
     </div>
     <?php
+      //login credentials for the mysql database
       $db_server = 'localhost:3308';
       $db_username = 'root';
       $db_password = '';
       $db_name = "sql_practice";
 
+      //tries to make a connection
       $conn = new mysqli($db_server, $db_username, $db_password, $db_name);
+
+      //checks to see if the request method is post
       if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+        //gets the sql statement from the textarea
         $sql = $_POST['sql-table'];
 
+        /*checks to see if the sql statement is select one by triming
+        the string and looking for a "S" in the first position
+        */
         if(strtoupper(trim($sql))[0] === 'S') {
+          //makes the sql selection query
           $result = $conn->query($sql);
           if($result->num_rows > 0) {
+            /*creates a html table with data from the
+            sql selection
+            */
             echo '<br> <table>';
+
+            //flag boolean variable
             $flag = true;
             while($row = $result->fetch_assoc()) {
+
+              //renders the column titles as a row
               if($flag) {
                 echo "<tr class='row-name'>";
                 $columns = array_keys($row);
@@ -52,6 +68,8 @@
                 $flag = false;
               }
               $row_echo = "<tr>";
+
+              //iterates over all the rows in the sql db
               foreach($row as $entry) {
                 $row_echo = $row_echo . '<th>' . $entry . '</th>';
               }
@@ -61,6 +79,7 @@
           }
         }
 
+        //for error messages related tot he query
         $error_msg;
 
         if($conn->query($sql)) {
@@ -74,6 +93,7 @@
       <div class="textarea-container">
         <textarea class='form-control' name='sql-table' > </textarea>
         <?php
+          //checks for the error messages and sql statement for the query
           if(isset($error_msg)) {
             echo '<br> <div class="alert alert-danger" role="alert"><strong>SQL ERROR</strong> '. $error_msg . ' </div>';
           }
@@ -87,9 +107,11 @@
     <h5> Tables </h5>
     <div class="table-title-container">
       <?php
+        //for showing the database tables
         $show = "SHOW TABLES FROM $db_name";
         $result = $conn->query($show);
         if($result) {
+          //each table
           while($row = $result->fetch_assoc()) {
             foreach($row as $entry) {
               echo '<span class="badge badge-secondary">' . $entry . '</span>';
